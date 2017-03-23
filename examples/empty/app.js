c7c698130c3100851e94106b4bc66a00b92ede68
Daniel Kostro <kostro.d@gmail.com>
2017-03-23 15:31:47 +0100

    add empty example

---
+++ examples/empty/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {ParallelCoordinates} from '../../src';
+
+ReactDOM.render(
+    <ParallelCoordinates />,
+    document.getElementById('example')
+);
